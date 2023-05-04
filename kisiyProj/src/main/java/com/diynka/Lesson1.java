2dd9a5489600709405401abf98d764b8c877d7ef
Dmitriy-95 <d_argunov@bk.ru>
2023-05-04 21:44:21 +0300

    add changes from lesson 1

---
+++ kisiyProj/src/main/java/com/diynka/Lesson1.java
@@ -0,0 +1,12 @@
+package com.diynka;
+
+public class Lesson1 {
+    public String getStr() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        Lesson1 lesson1 = new Lesson1();
+        System.out.println(lesson1.getStr());
+    }
+}
